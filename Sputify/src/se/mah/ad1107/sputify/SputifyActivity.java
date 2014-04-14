18eb2bbba13a3c3e0968c0b44cac10655e3ea6f3
Patrik Larsson <patriklarsson88@gmail.com>
2014-04-14 19:38:44 +0200

    Delade upp det i mindre metoder och ändrade lite anrop. Nu kan appen sätta igång bluetooth på mobilen.s

--- Sputify/src/se/mah/ad1107/sputify/SputifyActivity.java
+++ Sputify/src/se/mah/ad1107/sputify/SputifyActivity.java
@@ -1,6 +1,5 @@
 package se.mah.ad1107.sputify;
 
-import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
 import android.content.Intent;
 import android.os.Bundle;
@@ -16,7 +15,7 @@ import android.widget.Toast;
 public class SputifyActivity extends ActionBarActivity {
 	private BluetoothAdapter mBtAdapter;
 	private static final int REQUEST_ENABLE_BT = 1;
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -26,7 +25,11 @@ public class SputifyActivity extends ActionBarActivity {
 			getSupportFragmentManager().beginTransaction()
 					.add(R.id.container, new PlaceholderFragment()).commit();
 		}
-		enableBt(); // Försöker sätta igång Bluetooth
+		mBtAdapter = BluetoothAdapter.getDefaultAdapter(); // Hämtar en referens till mobilens bluetooth enhet
+		Toast.makeText(this, "Bluetooth avaiable: " + BluetoothAvaiable(), Toast.LENGTH_LONG).show();
+		if (BluetoothAvaiable()) {
+			EnableBluetooth();
+		}
 	}
 
 	@Override
@@ -65,30 +68,35 @@ public class SputifyActivity extends ActionBarActivity {
 			return rootView;
 		}
 	}
-	/** Metod för att sätta på bluetooth */
-	private void enableBt ()  {
-		mBtAdapter = BluetoothAdapter.getDefaultAdapter(); // Hämtar en referens till enhentens bluetooth adapter.
-		if(mBtAdapter == null){
-			// Enheten har inte support för bluettoth
-		}
-		else if(!mBtAdapter.isEnabled()) {
-			 // Kontrollerar om enhenten har bluetooth aktiverat eller inte
-				Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-				startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
-				onActivityResult(REQUEST_ENABLE_BT, Activity.RESULT_OK, enableBtIntent);
-		}
-	}
-	
-	@Override
-	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-		if(requestCode == REQUEST_ENABLE_BT && resultCode == Activity.RESULT_OK) {
-			mBtAdapter.enable();
-			Toast.makeText(this, "Bluetooth är igån", Toast.LENGTH_LONG).show(); // Visar ett meddelande som säger att BT har kommit igång
-		}
-		else if (requestCode == REQUEST_ENABLE_BT && resultCode == Activity.RESULT_CANCELED) {
-			Toast.makeText(this, "Bluetooth sattes inte igång",Toast.LENGTH_LONG ).show();
-		}
-	}
-	
 
+	/*
+	 * Metoden kontrollerar om mobilen har bluetooth eller inte
+	 */
+	private boolean BluetoothAvaiable() {
+		if (mBtAdapter == null) {
+			return false;
+		} else {
+			return true;
+		}
+	}
+
+	/*
+	 * Sätt igång bluetooth på enheten
+	 */
+	private void EnableBluetooth() {
+		if (BluetoothAvaiable() && !mBtAdapter.isEnabled()) {
+			Intent enableBtIntent = new Intent(
+					BluetoothAdapter.ACTION_REQUEST_ENABLE); // Skapar en intent
+																// som ska fråga
+																// användaren
+																// efter input.
+			startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+		}
+	}
+	@Override
+	public void onActivityResult(int requestCode, int resultCode, Intent data) {
+		if (requestCode == REQUEST_ENABLE_BT && resultCode == RESULT_OK) {
+			Toast.makeText(this, "Bluetooth turned on!", Toast.LENGTH_LONG).show();
+		}
+	}
 }
