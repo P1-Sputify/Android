9cde51a32b9826cd0bf51d59019f88250d315a7b
Patrik Larsson <patriklarsson88@gmail.com>
2014-04-14 19:07:22 +0200

    Har gjort så att appen kan aktivera bluetooth. Finns en bugg som gör att enheten sätter igång bluetooth innan användaren har blivit tillfrågrad.

--- Sputify/src/se/mah/ad1107/sputify/SputifyActivity.java
+++ Sputify/src/se/mah/ad1107/sputify/SputifyActivity.java
@@ -1,18 +1,22 @@
 package se.mah.ad1107.sputify;
 
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
+import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBarActivity;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.os.Build;
+import android.widget.Toast;
 
 public class SputifyActivity extends ActionBarActivity {
-
+	private BluetoothAdapter mBtAdapter;
+	private static final int REQUEST_ENABLE_BT = 1;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -22,6 +26,7 @@ public class SputifyActivity extends ActionBarActivity {
 			getSupportFragmentManager().beginTransaction()
 					.add(R.id.container, new PlaceholderFragment()).commit();
 		}
+		enableBt(); // Försöker sätta igång Bluetooth
 	}
 
 	@Override
@@ -60,5 +65,30 @@ public class SputifyActivity extends ActionBarActivity {
 			return rootView;
 		}
 	}
+	/** Metod för att sätta på bluetooth */
+	private void enableBt ()  {
+		mBtAdapter = BluetoothAdapter.getDefaultAdapter(); // Hämtar en referens till enhentens bluetooth adapter.
+		if(mBtAdapter == null){
+			// Enheten har inte support för bluettoth
+		}
+		else if(!mBtAdapter.isEnabled()) {
+			 // Kontrollerar om enhenten har bluetooth aktiverat eller inte
+				Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+				startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+				onActivityResult(REQUEST_ENABLE_BT, Activity.RESULT_OK, enableBtIntent);
+		}
+	}
+	
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		if(requestCode == REQUEST_ENABLE_BT && resultCode == Activity.RESULT_OK) {
+			mBtAdapter.enable();
+			Toast.makeText(this, "Bluetooth är igån", Toast.LENGTH_LONG).show(); // Visar ett meddelande som säger att BT har kommit igång
+		}
+		else if (requestCode == REQUEST_ENABLE_BT && resultCode == Activity.RESULT_CANCELED) {
+			Toast.makeText(this, "Bluetooth sattes inte igång",Toast.LENGTH_LONG ).show();
+		}
+	}
+	
 
 }
