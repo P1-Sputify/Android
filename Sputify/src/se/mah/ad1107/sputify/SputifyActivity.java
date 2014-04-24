25c04a3bdf88eeb0d46aa7b36d6203bf30c95ab9
Patrik Larsson <patriklarsson88@gmail.com>
2014-04-24 10:29:14 +0200

    Kan Skicka över Bluetooth till Dator

--- Sputify/src/se/mah/ad1107/sputify/SputifyActivity.java
+++ Sputify/src/se/mah/ad1107/sputify/SputifyActivity.java
@@ -1,8 +1,8 @@
 package se.mah.ad1107.sputify;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
-import java.util.Set;
 import java.util.UUID;
 
 import android.app.Activity;
@@ -14,10 +14,8 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ListView;
 import android.widget.TextView;
 
 public class SputifyActivity extends Activity {
@@ -38,12 +36,25 @@ public class SputifyActivity extends Activity {
 	private Button mConnectbutton;
 	private TextView mDeviceInfo;
 	private EditText mEditMessage;
+	private Button mConnectButton;
 	
 	private static final UUID MY_UUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB"); // Standard UUID för seriell kommunikation
 	private static String mServerAdress; // Mac adressen till server ( i detta fall vår bluetooth modul.)
 	
 	
 	private BluetoothDevice mSelectedDevice = null; // En pekarare till den valda enheten
+	
+	// Threads
+	private ConnectThread mConnectThread;
+	private manageConnectionThread mManagConnectionThread;
+	
+	
+	// Konstanter för bluetoothStates
+	private final int BT_STATE_CONNECTED = 1;
+	private final int BT_STATE_DISCONNECTED = 0;
+	private final int BT_STATE_CONNECTING = 2;
+	private final int BT_STATE_SENDING = 3;
+	
 	
 	
 	
@@ -79,6 +90,16 @@ public class SputifyActivity extends Activity {
 				sendData(message);
 			}
 		});
+		
+		mConnectbutton = (Button)findViewById(R.id.button_connectButton);
+		mConnectbutton.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				connect();
+				
+			}
+		});
 	}
 		
 	
@@ -108,37 +129,9 @@ public class SputifyActivity extends Activity {
 	 * Detta bör göras i en tråd
 	 */
 	public void connect() {
-		try {
-			btSocket = mSelectedDevice.createRfcommSocketToServiceRecord(MY_UUID); // Skapar en Bluetooth socket
-			Log.d(TAG, "Socket Creation");
-		} catch (IOException e) {
-			Log.d(TAG, "Socket Creation Failed" + "\nMacAdress: " + mServerAdress + "\nUUID: " + MY_UUID +"\n" + e.getMessage());
-			// Felmeddelande
-		}
-		
-		mBluetoothAdapter.cancelDiscovery(); // Discovry är krävande så den ska vara avstängd innan man försöker ansluta
-		
-		try{
-			btSocket.connect(); // Försöker ansluta till den valda enheten. Detta anrop kommer att blockera appaen tills att den lyckas eller misslyckas. Lägg i thread när vi får det att fungera
-			Log.d(TAG, "Trying to connect to the bluetooth device"); // Kommer att blockerar programet tills den connectar. Därför bör detta läggas i en thread.
-		} catch(IOException e) {
-			try {
-				Log.d(TAG, "Closing Socket....");
-				btSocket.close();
-			} catch (Exception e2) {
-				Log.d(TAG, "Connection failed and could not close socket" + e2.getMessage());
-				finish(); // Krash i programmet
-			}
-			
-		}
-		
-		Log.d(TAG, "...Creating out Stream....");
-		try {
-			outStream = btSocket.getOutputStream(); // Försöker att skapa strömmen som ska skicka data imellan.
-		} catch(IOException e) {
-			Log.d(TAG, "outputStream creation failed");
-			finish();
-		}
+		mConnectThread = new ConnectThread(mSelectedDevice);
+		mConnectThread.run();
+		mManagConnectionThread = new manageConnectionThread(mConnectThread.mmSocket);
 	}
 	
 	/**
@@ -148,12 +141,7 @@ public class SputifyActivity extends Activity {
 	 * 		En sträng med det man vill skicka
 	 */
 	private void sendData(String message) {
-		byte[] msgBuffer = message.getBytes(); // Skapar en buffer för att kunna skicka meddelandet
-		try {
-			outStream.write(msgBuffer);
-		} catch (IOException e) {
-			Log.d(TAG, "Exception thron while writing data" + e.getMessage());
-		}
+		mManagConnectionThread.write(message);
 	}
 	
 	private void selectBTDevice() {
@@ -169,7 +157,6 @@ public class SputifyActivity extends Activity {
 			Log.d(TAG, mSelectedDevice.getAddress());
 			if(mSelectedDevice != null) {
 				mDeviceInfo.setText(mSelectedDevice.toString());
-				connect();
 			}
 			
 			switch (requestCode) {
@@ -188,6 +175,90 @@ public class SputifyActivity extends Activity {
 		}
 	}
 	
+	private class ConnectThread extends Thread {
+		private final BluetoothSocket mmSocket;
+		private final BluetoothDevice mmDevice;
+		
+		public ConnectThread(BluetoothDevice device) {
+			// Använd ett temporärt objekt som sennare kommer att tilldelas mmSocket
+			BluetoothSocket tmp = null;
+			mmDevice = device;
+			
+			// Försök att skapa en socket
+			try {
+				tmp = mmDevice.createRfcommSocketToServiceRecord(MY_UUID); // Skapar en Bluetooth socket
+				Log.d(TAG, "Socket Creation");
+			} catch (IOException e) {
+				Log.d(TAG, "Socket Creation Failed" + "\nMacAdress: " + mServerAdress + "\nUUID: " + MY_UUID +"\n" + e.getMessage());
+				// Felmeddelande
+			}
+			mmSocket = tmp;
+		}
+		
+		@Override
+		public void run() {
+			try{
+				mmSocket.connect(); // Försöker ansluta till den valda enheten. Detta anrop kommer att blockera appaen tills att den lyckas eller misslyckas. Lägg i thread när vi får det att fungera
+				Log.d(TAG, "Trying to connect to the bluetooth device"); // Kommer att blockerar programet tills den connectar. Därför bör detta läggas i en thread.
+			} catch(IOException e) {
+				try {
+					Log.d(TAG, "Closing Socket....");
+					mmSocket.close();
+				} catch (Exception e2) {
+					Log.d(TAG, "Connection failed and could not close socket" + e2.getMessage());
+					finish(); // Krash i programmet
+				}
+			}
+		}
+		
+		public void cancel() {
+			try{
+				mmSocket.close();
+			} catch(IOException e) {
+				//Felmeddlenade
+			}
+		}
+	}
+	
+	private class manageConnectionThread extends Thread {
+		private final BluetoothSocket mmSocket;
+		private final InputStream mmInStream;
+		private final OutputStream mmOutputStream; // För denna sprinten behövs förmodligen inte denna
+		
+		public manageConnectionThread(BluetoothSocket socket) {
+			mmSocket = socket;
+			InputStream tmpInStream = null;
+			OutputStream tmpOutStream = null;
+			try
+			{
+				tmpInStream = socket.getInputStream();
+				tmpOutStream = socket.getOutputStream();
+			} catch(IOException e) {
+				//FelMeddelande
+			}
+			mmInStream = tmpInStream;
+			mmOutputStream = tmpOutStream;
+		}
+		
+		public void run() {
+			byte[] buffer = new byte [1024];
+		}
+		
+		public void write(String message) {
+			byte[] buffer = message.getBytes();
+			try{
+				mmOutputStream.write(buffer);
+			} catch(IOException e) {
+				// Visa felmeddleande
+			}
+		}
+		
+		public void cancel() {
+			try{
+				mmSocket.close();
+			} catch (IOException e) {};
+		}
+	}
 }
 
 
